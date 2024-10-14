5f3432c3450cdc5d98221ab01e6b02aee9ed67d8
Jose Pereira <Jose.Pereira1@ibm.com>
2024-10-14 16:09:51 +0700

    Chatbot integration.

--- client/src/components/Watson/WatsonChat.js
+++ client/src/components/Watson/WatsonChat.js
@@ -3,7 +3,7 @@ import React, { useEffect } from 'react';
 const WatsonChat = () => {
     useEffect(() => {
         window.watsonAssistantChatOptions = {
-            integrationID: "5a7e7285-94ea-450d-b78f-678dc7c73825",
+            integrationID: "288caf73-2f9d-470e-a97b-b16cf2937b07",
             region: "jp-tok",
             serviceInstanceID: "a43f7dc4-4e95-4bc5-9e42-0d5924b21929",
             onLoad: async (instance) => { await instance.render(); }
